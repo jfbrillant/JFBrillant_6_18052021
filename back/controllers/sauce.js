a5ba51f023d5fac67a45eb888f278a0841cf4141
JF Brillant <jf.brillant@live.fr>
2021-06-07 21:18:04 +0200

    Fix CRUD

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,14 +1,14 @@
-const Thing = require('../models/Sauce');
+const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.thing);
+  const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
-  thing.save()
+  sauce.save()
     .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
     .catch(error => res.status(400).json({ error }));
 };
--- back/models/Sauce.js
+++ back/models/Sauce.js
@@ -7,11 +7,11 @@ const sauceSchema = mongoose.Schema({
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
-  heat: { type: String, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: { type: [String], required: true },
-  usersDisliked: { type: [String], required: true }
+  heat: { type: Number, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] }
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
